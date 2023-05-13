a4744cd6462bea15ef3cc866b93cf6d6e154dd96
kirmich <sadkovk@yandex.ru>
2023-05-13 14:23:36 +0300

    fh

--- kp.py
+++ kp.py
@@ -1,38 +1,44 @@
 from kinopoisk_api import KP
+from bs4 import BeautifulSoup
+import requests
+import lxml
 import pandas as pd
 
 kinopoisk = KP(token='be5114b9-31c7-4d3d-b88e-874fb3cf8f11')
-print(kinopoisk.about, kinopoisk.version)
 
-spisok = []
-
+spisok_film = []
+spisok_siries = []
 with open("spisok.txt", "r") as file:
     for line in file:
         if line[25:29] == 'film':
-            print(line[30:-2])
-            spisok.append(line[30:-2])
-        else: print(line)
+            spisok_film.append(line[30:-2])
+        if line[25:31] == 'series':
+            spisok_siries.append(line[:-2])
 
-# data = []
-#
-# for i in spisok:
-#     a = kinopoisk.get_film(i)
-#     b = [a.ru_name,
-#          a.name,
-#          a.year,
-#          a.duration,
-#          a.genres,
-#          a.countries,
-#          a.kp_rate,
-#          a.imdb_rate,
-#          a.description]
-#     data.append(b)
-#
-# col = ['Название', 'Оригинальное название', 'Год', 'Продолжительность', 'Жанр', 'Страна', 'Кинопоиск', 'IMDB', 'Описание']
-#
-# df = pd.DataFrame(data, columns=col)
-#
-# print(df)
-#
-# df.to_excel('./film.xlsx')
+
+
+
+
+data_f = []
+
+for i in spisok_film:
+    a = kinopoisk.get_film(i)
+    b = [a.ru_name,
+         a.name,
+         a.year,
+         a.duration,
+         a.genres,
+         a.countries,
+         a.kp_rate,
+         a.imdb_rate,
+         a.description]
+    data_f.append(b)
+
+col = ['Название', 'Оригинальное название', 'Год', 'Продолжительность', 'Жанр', 'Страна', 'Кинопоиск', 'IMDB', 'Описание']
+
+df_film = pd.DataFrame(data_f, columns=col)
+
+print(df_film)
+
+df_film.to_excel('./film.xlsx')
 
